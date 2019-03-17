0a6ff8dcafe37c38452db41d18cb306287b842e8
marthyndl <marthyndl@gmail.com>
2019-03-17 15:09:03 -0300

    Add properties in Person component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name='Martin' age='30' />
+        <Person name='Daniel' age='29' />
+        <Person name='Sabrina' age='27' />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'));
