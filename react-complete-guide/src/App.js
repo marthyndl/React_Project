392e6a60675e541582f4513685b88c909a56203b
marthyndl <marthyndl@gmail.com>
2019-03-17 17:49:59 -0300

    Adding two way Binding and using of styling with stylesheet and working with inline styles

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -24,28 +24,49 @@ class App extends Component {
     })
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Martin', age:30 },
+        { name: event.target.value, age:30 },
+        { name:'Sabrina', age:27 }
+      ] 
+    })
+  }
+
   render() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working</p>
-      <button onClick={() => this.switchNameHandler('Martin Daniel')} >Switch name</button>
-      <Person 
-      name={this.state.persons[0].name} 
-      age={this.state.persons[0].age} 
-      click={this.switchNameHandler.bind(this, 'Martin')}
-      >My hobbies: Soccer</Person>
-      <Person 
-      name={this.state.persons[1].name} 
-      age={this.state.persons[1].age} />
-      <Person 
-      name={this.state.persons[2].name} 
-      age={this.state.persons[2].age} />
-      {/* <Person name='Martin' age='30'>My hobbies: Soccer</Person>
-      <Person name='Daniel' age='29' />
-      <Person name='Sabrina' age='27' /> */}
-    </div>
-  )
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+    
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working</p>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Martin Daniel')} >Switch name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} 
+        click={this.switchNameHandler.bind(this, 'Martin')}
+        >My hobbies: Soccer</Person>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} 
+        change={this.nameChangeHandler}/>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age} />
+        {/* <Person name='Martin' age='30'>My hobbies: Soccer</Person>
+        <Person name='Daniel' age='29' />
+        <Person name='Sabrina' age='27' /> */}
+      </div>
+    )
   }
 }
 
