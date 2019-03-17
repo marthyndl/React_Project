ecaae595dd1bd8a021189ce6856291b7ef4f2de8
marthyndl <marthyndl@gmail.com>
2019-03-17 15:54:09 -0300

    add state props and switchNameHandler in Person Component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name:'Martin', age:30 },
+      { name:'Daniel', age:29 },
+      { name:'Sabrina', age:27 }
+    ]
+  }
+  
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person name='Martin' age='30'>My hobbies: Soccer</Person>
+        <button onClick={this.switchNameHandler} >Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons.age[0]} >My hobbies: Soccer</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons.age[1]} />
+        <Person name={this.state.persons[2].name} age={this.state.persons.age[2]} />
+        {/* <Person name='Martin' age='30'>My hobbies: Soccer</Person>
         <Person name='Daniel' age='29' />
-        <Person name='Sabrina' age='27' />
+        <Person name='Sabrina' age='27' /> */}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'));
