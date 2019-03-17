ab7b86993c8570944314cbd5412e431143dfb5f7
marthyndl <marthyndl@gmail.com>
2019-03-17 17:23:00 -0300

    Passing Methods another component and reutrning with out Hooks for standard state in Class component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,51 +1,53 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name:'Martin', age:30 },
       { name:'Daniel', age:29 },
       { name:'Sabrina', age:27 }
     ],
     otherState: 'some other value'
-  });
+  }
   
-  const switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS this.state.persons[0].name = 'Martin Daniel';
-    setPersonsState({
+    this.setState({
       persons: [
-        { name:'Martin Daniel', age:30 },
+        { name: newName, age:30 },
         { name:'Daniel', age:30 },
         { name:'Sabrina', age:27 }
       ] 
     })
   }
 
+  render() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working</p>
-      <button onClick={switchNameHandler} >Switch name</button>
+      <button onClick={() => this.switchNameHandler('Martin Daniel')} >Switch name</button>
       <Person 
-      name={personsState.persons[0].name} 
-      age={personsState.persons[0].age} 
-      click={switchNameHandler}
+      name={this.state.persons[0].name} 
+      age={this.state.persons[0].age} 
+      click={this.switchNameHandler.bind(this, 'Martin')}
       >My hobbies: Soccer</Person>
       <Person 
-      name={personsState.persons[1].name} 
-      age={personsState.persons[1].age} />
+      name={this.state.persons[1].name} 
+      age={this.state.persons[1].age} />
       <Person 
-      name={personsState.persons[2].name} 
-      age={personsState.persons[2].age} />
+      name={this.state.persons[2].name} 
+      age={this.state.persons[2].age} />
       {/* <Person name='Martin' age='30'>My hobbies: Soccer</Person>
       <Person name='Daniel' age='29' />
       <Person name='Sabrina' age='27' /> */}
     </div>
   )
+  }
 }
 
-export default app;
+export default App;
 
