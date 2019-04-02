e505ce1eea321208d8ae9b3954cd2149276b4fb2
marthyndl <marthyndl@gmail.com>
2019-04-02 20:12:32 -0300

    Outputting Persons

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -53,22 +53,13 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (
-        <div>          
-          <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} 
-          click={this.switchNameHandler.bind(this, 'Martin')}
-          >My hobbies: Soccer</Person>
-          <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age} 
-          change={this.nameChangeHandler}/>
-          <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
-          {/* <Person name='Martin' age='30'>My hobbies: Soccer</Person>
-          <Person name='Daniel' age='29' />
-          <Person name='Sabrina' age='27' /> */}
+        <div>     
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name}
+              age={person.age}
+            />
+          })}
         </div>
       );
     }
